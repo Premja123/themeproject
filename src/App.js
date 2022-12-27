96a1796064ae647435e21c7c7ea6bd7155647c92
premja123 <nikhil.gupta56@t-mobile.com>
2022-12-27 14:41:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <h1>Theme Builder</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// 1: Import
+import React, { useState, useEffect } from 'react';
+import styled, { ThemeProvider } from "styled-components";
+import WebFont from 'webfontloader';
+import { GlobalStyles } from './GlobalStyles';
+import {useTheme} from './useTheme';
+import ThemeSelector from './ThemeSelector';
+
+// 2: Create a cotainer
+const Container = styled.div`
+  margin: 5px auto 5px auto;
+`;
 
 function App() {
+
+  const {theme, themeLoaded, getFonts} = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+   }, [themeLoaded]);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts()
+      }
+    });
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   {
+      themeLoaded && <ThemeProvider theme={ selectedTheme }>
+        <GlobalStyles/>
+        <Container style={{fontFamily: selectedTheme.font}}>
+          <h1>Theme Builder</h1>
+          <p>
+            This is a theming system with a Theme Switcher and Theme Builder.
+            Do you want to see the source code? <a href="https://github.com/atapas/theme-builder" target="_blank">Click here.</a>
+          </p>
+          <ThemeSelector setter={ setSelectedTheme } />
+        </Container>
+        </ThemeProvider>
+    }
+    
+    </>
   );
 }
 
